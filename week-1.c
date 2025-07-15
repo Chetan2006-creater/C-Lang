587d3ed9dbac4e9600d3e4e89468734c1930c618
Chetan2006-creater <dchetansivapavankumar@gmail.com>
2025-07-15 12:14:07 +0530

    Create week-1.c

---
+++ week-1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+// write the addition of two numbers 
+/*
+int main(){
+int a,b,c ;
+printf("Enter the three number\n");
+scanf("%d,%d,%d",&a,&b,&c);
+printf("  Sum of the numbers %d", a+b+c);
+return 0;
+}
+*/
+/*
+int main(){
+int a ;
+printf("Enter a number\n");
+scanf("%d",&a);
+printf("octa decimal number is %o\n",a);
+printf("hexadecimal number is %X\n",a);
+return 0;
+}
+*/
